4cae25a174cd3a37241bbcbd4c8784f09be9a1e7
tienpm <tienphamminh0312@gmail.com>
2023-04-11 21:52:09 +0700

    Add functions

--- includes/connect.php
+++ includes/connect.php
@@ -4,3 +4,21 @@ if (!defined('_INCODE')) {
     http_response_code(403);
     exit;
 }
+
+/*Create a connection to MySQL using PDO*/
+try {
+    if (class_exists('PDO')) {
+        $dsn = _DRIVER . ':dbname=' . _DBNAME . ';host=' . _HOST; // $dsn: Data Source Name
+        $options = [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // Set the PDO error mode
+        ];
+
+        // Create connection
+        $dbh = new PDO($dsn, _USERNAME, _PASSWORD, $options); // $dbh: database handle
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage() . '<br>';
+    echo 'File: ' . $e->getFile() . ' - Line: ' . $e->getLine();
+    exit;
+}
--- includes/session.php
+++ includes/session.php
@@ -4,3 +4,81 @@ if (!defined('_INCODE')) {
     http_response_code(403);
     exit;
 }
+
+/*Write functions to handle session*/
+
+function setSession($key, $value): bool
+{
+    if (!empty(session_id())) {
+        $_SESSION[$key] = $value;
+        return true;
+    }
+
+    return false;
+}
+
+function getSession($key = '')
+{
+    if (empty($key)) {
+        return $_SESSION;
+    } elseif (isset($_SESSION[$key])) {
+        return $_SESSION[$key];
+    }
+
+    return false;
+}
+
+// Unset session variables
+function removeSession($key = ''): bool
+{
+    if (empty($key)) {
+        session_unset();
+        return true;
+    } elseif (isset($_SESSION[$key])) {
+        unset($_SESSION[$key]);
+        return true;
+    }
+
+    return false;
+}
+
+// Terminate the session
+function destroySession(): void
+{
+    // Unset all the session variables.
+    $_SESSION = array();
+
+    // Delete the session cookie.
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+
+    // Finally, destroy the session.
+    session_destroy();
+}
+
+// Set flash data
+function setFlashData($key, $value): bool
+{
+    $key = 'flash_' . $key;
+    return setSession($key, $value);
+}
+
+// Get flash data
+function getFlashData($key)
+{
+    $key = 'flash_' . $key;
+    $data = getSession($key);
+    removeSession($key);
+
+    return $data;
+}
